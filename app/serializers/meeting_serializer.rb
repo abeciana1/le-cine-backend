e64c7c29d8a787e443e5452e6f1d14ffe39298e2
Alex <alex.beciana@gmail.com>
2020-10-15 00:43:23 -0400

    Updated MeetingSerializer for frontend fetch request access to movies

--- app/serializers/meeting_serializer.rb
+++ app/serializers/meeting_serializer.rb
@@ -1,5 +1,6 @@
 class MeetingSerializer < ActiveModel::Serializer
   attributes :id, :date, :time, :link, :title, :about, :excerpt, :club_id
+  has_many :movie_meetings
   has_many :movies
   has_one :club
 end
